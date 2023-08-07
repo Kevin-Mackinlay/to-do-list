749de0d5f82054e0536b04d9047c54604f12b0f5
Mackinlay Kevin <kemack83@gmail.com>
2023-08-06 23:24:32 -0300

    adding code: buttons

--- js/archivo.js
+++ js/archivo.js
@@ -1,7 +1,7 @@
 //first bring the elements
 let formTarea = document.querySelector("#formTarea");
 let inputTarea = document.querySelector("#inputTarea");
-let tareaContainer = document.querySelector("#tareaContainer");
+let tareasContenedor = document.querySelector("#tareasContenedor");
 
 //design empty variable
 let tareas;
@@ -17,18 +17,59 @@ if (localStorage.getItem("tareas") == null) {
 //primer evento
 
 formTarea.onsubmit = (event) => {
-    event.preventDefault();
-    let nuevaTarea = {
-        id: Date.now().toString(36),
-        tarea: inputTarea.value,
-        estado: false,
+  event.preventDefault();
+  let nuevaTarea = {
+    id: Date.now().toString(36),
+    tarea: inputTarea.value,
+    estado: false,
+  };
+  //guardamos en el array
+  tareas.push(nuevaTarea);
+
+  //guardar en el localStorage
+  localStorage.setItem("tareas", JSON.stringify(tareas));
+  console.log(tareas);
+  mostrarTareas();
+  formTarea.reset();
+};
+
+//funcion y creacion de elementos ("div") y sus clases
+
+const mostrarTareas = () => {
+  tareasContenedor.innerHTML = " ";
+
+  tareas.forEach((tarea, index) => {
+    let tareaBox = document.createElement("div");
+    tareaBox.classList.add("d-flex", "mt-3", "justify-content-between", "align-items-center");
+    tareasContenedor.appendChild(tareaBox);
+
+    let tareaTexto = document.createElement("p");
+    tareaTexto.classList.add("align-self-end");
+
+    if (tareas[index].estado === true) {
+      tareaTexto.classList.add("text-decoration-line-through");
     }
-//guardamos en el array
-tareas.push(nuevaTarea);
+    tareaTexto.innerHTML = tarea.tarea;
+    tareaBox.appendChild(tareaTexto);
 
-//guardar en el localStorage
-localStorage.setItem("tareas", JSON.stringify(tareas));
-console.log(tareas);
-formTarea.reset();
+    let div = document.createElement("div");
+    tareaBox.appendChild(div);
 
-}
+    //boton eliminar
+
+    let btnEliminar = document.createElement("button");
+    btnEliminar.classList.add("btn", "btn-danger", "ms-2");
+    btnEliminar.innerHTML = "Eliminar";
+    div.appendChild(btnEliminar);
+
+    
+    //boton editar
+
+    let btnEditar = document.createElement("button");
+    btnEditar.classList.add("btn", " btn-success", "ms-2");
+    btnEditar.innerHTML = "Editar";
+    div.appendChild(btnEditar);
+  });
+};
+
+mostrarTareas();
